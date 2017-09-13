31e207a2bdd7c1bd4bf50c9786fca0bdd7c229bd
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-09-13 16:07:01 -0500

    display current movies to table in the document object.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+// import sayHello from './hello';
+// sayHello('World');
 
 /**
  * require style imports
  */
+
+///////////////////////////////////////////////
+///////////////// variables ///////////////////
+///////////////////////////////////////////////
 const getMovies = require('./getMovies.js');
 
+let tableBody = document.getElementById("movie-table");
+
+///////////////////////////////////////////////
+/////////////// add movie form ////////////////
+///////////////////////////////////////////////
+
+
+
+
+
+///////////////////////////////////////////////
+/////////// movie creation table //////////////
+///////////////////////////////////////////////
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+
+  let msg = "";
+  movies.forEach((item) => {
+        msg = "<tr><td>" + "id: " + item.id + "</td><td>" + "title: " + item.title + "</td><td>" + "rating: " + item.rating + "</td></tr>"
+    + msg}
+  );
+    tableBody.innerHTML = msg;
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
