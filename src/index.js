2359bf6d3f9729fb7d4be8d0715519fbb355d757
ironicsushi <yuli5565@colorado.edu>
2017-09-14 14:27:29 -0500

    *added an edit option to allow user to change movie entries

--- src/index.js
+++ src/index.js
@@ -17,9 +17,11 @@ const getMovies = require('./getMovies.js');
 let tableBody = document.getElementById("movie-table");
 let addButton = document.getElementById("add-button");
 let titleInput = document.getElementById("title");
+let editTitleInput = document.getElementById("edit-title");
+let editButton = document.getElementById("edit-button");
+let deleteButton = document.getElementById("delete");
 let userRating = document.querySelectorAll(".user-rating");
-
-console.log(userRating);
+let editUserRating = document.querySelectorAll(".edit-user-rating");
 
 ///////////////////////////////////////////////
 /////////////// add movie form ////////////////
@@ -41,15 +43,48 @@ addButton.addEventListener("click", (e) => {
       let inputMovie = {
         title: titleInput.value,
         rating: radioValue
-
-
       };
 
-      addMovies(inputMovie).then((savedMovie) => console.log(savedMovie));
+      addMovies(inputMovie).then((savedMovie) => /* attach the listener here */ console.log(savedMovie));
 
       console.log(inputMovie);
 });
 
+///////////////////////////////////////////////
+/////////// movie EDIT option /////////////////
+///////////////////////////////////////////////
+
+
+    let moviesButton = document.getElementsByClassName("movies");
+    console.log(moviesButton);
+    for (let movie of moviesButton) {
+
+        movie.addEventListener("click", (e) => {
+          let movieInfo = e.target.parentElement.parentElement.children;// tds
+
+            let id = movieInfo[0].innerHTML;
+            let title = movieInfo[1].innerHTML;
+            let rating = movieInfo[2].innerHTML;
+            editTitleInput.value = title;
+
+            console.log(typeof rating);
+
+            for (let i = 1; i <= userRating.length; i++) {
+              if (rating == i) {
+                  editUserRating[i - 1].checked = true;
+                  console.log(i);
+
+              }
+            }
+
+        });
+
+    }
+
+
+      editButton.addEventListener("click", (e) => {
+
+      });
 
 
 ///////////////////////////////////////////////
@@ -57,21 +92,29 @@ addButton.addEventListener("click", (e) => {
 ///////////////////////////////////////////////
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-
+  console.log(movies);
   let msg = "";
+
   movies.forEach((item) => {
         msg =
             "<tr>" +
-            "<td>" + "id: " + item.id +
-            "</td><td>" + "title: " + item.title +
-            "</td><td>" + "rating: " + item.rating +
-            "</td>" + "<button>Delete</button>" +
-            "<td>" + "<button>Edit</button>" +
+            "<td>" + item.id +
+            "</td><td>" + item.title +
+            "</td><td>" + item.rating +
+            "</td>" + "<input type = 'submit' id = 'delete' value='Delete'>" +
+            "<td>" + "<input type = 'submit' class='movies' id = 'edit' value='Edit'>" +
             "</tr>"
     + msg}
   );
     tableBody.innerHTML = msg;
+
+
+    // attach the listener here
+
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+
+
+
